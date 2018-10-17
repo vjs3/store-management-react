5dbce1f86892dfaf3599e9e1da12551fffcb062e
Vishwajeet Srivastava <vishwajeets912@gmail.com>
2018-10-17 16:25:51 +0200

    created form inside store picker

--- src/components/StorePicker.js
+++ src/components/StorePicker.js
@@ -2,7 +2,13 @@ import React from 'react';
 
 class StorePicker extends React.Component { 
     render() { 
-        return <p>Hello React world!</p>
+        return (
+            <form className="store-selector">
+                <h2>Please enter A Store</h2>
+                <input type="text" required placeholde="Store Name" />
+                <button type="submit">Visit Store -> </button>
+            </form>
+        )
     }
 }
 
