24c8a0b0af25b9b9fd7bb01e5a615b30e0f46c9d
Vishwajeet Srivastava <vishwajeets912@gmail.com>
2018-10-18 16:02:27 +0200

    added routing for different scenarios

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 // let's go!
 import React from 'react';
 import { render } from 'react-dom';
-import App from './components/App';
+import Router from './components/Router';
 import "./css/style.css"
 
-render(<App />, document.querySelector('#main'));
+render(<Router />, document.querySelector('#main'));
