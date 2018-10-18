f988fb40ba4d488f81f4c1ed6b4e8b9d5995e569
Vishwajeet Srivastava <vishwajeets912@gmail.com>
2018-10-18 17:06:14 +0200

    connected store picker to app using routing

--- src/components/StorePicker.js
+++ src/components/StorePicker.js
@@ -1,15 +1,29 @@
-import React from 'react';
+import React from "react";
+import { getFunName } from "../helpers";
 
-class StorePicker extends React.Component { 
-    render() { 
-        return (
-            <form className="store-selector">
-                <h2>Please enter A Store</h2>
-                <input type="text" required placeholder ="Store Name" />
-                <button type="submit">Visit Store -> </button>
-            </form>
-        )
-    }
+class StorePicker extends React.Component {
+  myInput = React.createRef();
+
+  goToStore = event => {
+    event.preventDefault();
+    const storeName = this.myInput.value.value;
+    this.props.history.push(`/store/${storeName}`);
+  };
+  render() {
+    return (
+      <form className="store-selector" onSubmit={this.goToStore}>
+        <h2>Please enter A Store</h2>
+        <input
+          type="text"
+          ref={this.myInput}
+          required
+          placeholder="Store Name"
+          defaultValue={getFunName()}
+        />
+        <button type="submit">Visit Store -> </button>
+      </form>
+    );
+  }
 }
 
 export default StorePicker;
