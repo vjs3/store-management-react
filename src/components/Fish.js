c0170413ed4da933363e7b79d12a0015befe4ddb
Vishwajeet Srivastava <vishwajeets912@gmail.com>
2018-10-20 22:54:17 +0200

    price formating using the helper method

--- src/components/Fish.js
+++ src/components/Fish.js
@@ -1,13 +1,15 @@
 import React from "react";
+import { formatPrice } from "../helpers";
 
 class Fish extends React.Component {
   render() {
-    const { image, name, price, description, status} = this.props.details;
+    const { image, name, price, description, status } = this.props.details;
     return (
       <li className="menu-fish">
         <img src={image} alt={name} />
-        <h3 className="fish-name">{name}
-            <span className="name">{price}</span>
+        <h3 className="fish-name">
+          {name}
+          <span className="name">{formatPrice(price)}</span>
         </h3>
         <p>{this.props.details.desc}</p>
         <p>{this.props.details.price}</p>
