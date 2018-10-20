152d665322c9a2701037ad4b598aa3286bd2c5f2
Vishwajeet Srivastava <vishwajeets912@gmail.com>
2018-10-20 22:51:32 +0200

    added further data for name, description

--- src/components/Fish.js
+++ src/components/Fish.js
@@ -5,6 +5,12 @@ class Fish extends React.Component {
     return (
       <li className="menu-fish">
         <img src={this.props.details.image} alt={this.props.details.name} />
+        <h3 className="fish-name">{this.props.details.name}
+            <span className="name">{this.props.details.price}</span>
+        </h3>
+        <p>{this.props.details.desc}</p>
+        <p>{this.props.details.price}</p>
+        <p>{this.props.details.status}</p>
       </li>
     );
   }
