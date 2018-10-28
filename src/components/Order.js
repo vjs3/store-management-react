4feb6f87336917d6e34ce7d0bbbff668eb9e9eb2
Vishwajeet Srivastava <vishwajeets912@gmail.com>
2018-10-28 20:45:29 +0100

    added logic for sum total of order

--- src/components/Order.js
+++ src/components/Order.js
@@ -2,7 +2,22 @@ import React from "react";
 
 class Order extends React.Component {
   render() {
-    return <div className="order">Order!!!</div>;
+    const orderIds = Object.keys(this.props.order);
+    // console.log(orderIds);
+    const total = orderIds.reduce((prevTotal, key) => {
+      const fish = this.props.fishes[key];
+      const count = this.props.order[key];
+      const isAvailable = fish && fish.status === "available";
+      if (isAvailable) {
+        return prevTotal + count * fish.price;
+      }
+    }, 0);
+    return (
+      <div className="order-wrap">
+        <h2>Order</h2>
+        {total}
+      </div>
+    );
   }
 }
 
