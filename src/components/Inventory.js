c63726140be52fa273626ab24b826a8397d48df9
Vishwajeet Srivastava <vishwajeets912@gmail.com>
2018-10-31 18:53:57 +0100

    added auth using twitter and github

--- src/components/Inventory.js
+++ src/components/Inventory.js
@@ -1,8 +1,10 @@
 import React from "react";
+import firebase from "firebase";
 import PropTypes from "prop-types";
 import AddFishForm from "./AddFishForm";
 import EditFishForm from "./EditFishForm";
 import Login from "./Login";
+import { firebaseApp } from "../base";
 
 class Inventory extends React.Component {
   static propTypes = {
@@ -12,9 +14,18 @@ class Inventory extends React.Component {
     loadSampleFishes: PropTypes.func
   };
 
-  authenticate = () => {
-    alert("hey");
+  authHandler = async authData => {
+    console.log(authData);
   };
+
+  authenticate = provider => {
+    const authProvider = new firebase.auth[`${provider}AuthProvider`]();
+    firebaseApp
+      .auth()
+      .signInWithPopup(authProvider)
+      .then(this.authHandler);
+  };
+
   render() {
     return <Login authenticate={this.authenticate} />;
     return (
